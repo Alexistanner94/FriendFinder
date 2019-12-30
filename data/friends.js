42e1cb882c6846eddd15a44cbc8d45a42ba0626d
Alexis Tanner <alexistanner94@hotmail.com>
2019-12-30 01:46:32 -0500

    added friends, routes etc

---
+++ data/friends.js
@@ -0,0 +1,33 @@
+var friends = [
+  {
+    name: "Lena",
+    photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYJtUSKswARugpPWKFD6gTOje0MHsmGDTItdMaR0WTr9PJ5JZ6&s",
+    scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+  },
+  {
+    name: "Zoe",
+    photo:
+      "https://i.pinimg.com/originals/61/01/7b/61017b275244c90f0ddf6c6fb0a2b319.jpg",
+    scores: [1, 1, 4, 2, 2, 3, 2, 5, 4, 1]
+  },
+  {
+    name: "Vanessa",
+    photo:
+      "https://i.pinimg.com/564x/98/b3/61/98b3614dc634b2e63700ad1cc977d2ea.jpg",
+    scores: [2, 3, 1, 4, 1, 3, 1, 5, 1, 1]
+  },
+  {
+    name: "Pam",
+    photo: "https://miro.medium.com/max/4692/1*zOz85HSX0a8RvJ0DTLeO9g.jpeg",
+    scores: [3, 4, 5, 2, 3, 5, 5, 4, 4, 2]
+  },
+  {
+    name: "Kenya",
+    photo:
+      "https://i.pinimg.com/564x/a0/24/29/a02429961f6923bbffbc7eceacf06f55.jpg",
+    scores: [3, 2, 3, 1, 1, 3, 1, 2, 2, 1]
+  }
+];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 var express = require("express");
+
 var path = require("path");
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("app/public"));
+
+require("./route/apiRoutes")(app);
+require("./route/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("Listening on PORT:" + PORT);
+});
