29a88db470573b348057ebf37f423b50458b9947
Alexis Tanner <alexistanner94@hotmail.com>
2020-01-02 11:26:51 -0500

    added scores

--- data/friends.js
+++ data/friends.js
@@ -3,31 +3,31 @@ var friends = [
     name: "Eva",
     photo:
       "https://www.essence.com/wp-content/uploads/2019/03/Screen-Shot-2019-03-19-at-11.28.19-AM-1920x1080.png?width=1920&height=1080",
-    scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+    scores: [3, 2, 3, 1, 1, 3, 1, 2, 2, 1]
   },
   {
     name: "Marlow",
     photo:
       "http://thejasminebrand.com/wp-content/uploads/2018/11/Screen-Shot-2018-11-05-at-10.57.56-AM.png",
-    scores: [1, 1, 4, 2, 2, 3, 2, 5, 4, 1]
+    scores: [3, 4, 5, 2, 3, 5, 5, 4, 4, 2]
   },
   {
     name: "Cynthia",
     photo:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQeQBuXT4Y_q0Vvsnzs_c4ilD-VvKOFX04pAIniFgEqlbOoYHCh",
-    scores: [2, 3, 1, 4, 1, 3, 1, 5, 1, 1]
+    scores: [2, 3, 1, 4, 2, 3, 1, 5, 1, 1]
   },
   {
     name: "Kandi",
     photo:
       "https://akns-images.eonline.com/eol_images/Entire_Site/2019104/rs_1024x759-191104113208-1024-Kandi_Burruss.jpg?fit=inside|900:auto&output-quality=90",
-    scores: [3, 4, 5, 2, 3, 5, 5, 4, 4, 2]
+    scores: [1, 1, 4, 2, 2, 3, 2, 5, 4, 1]
   },
   {
     name: "Porshe",
     photo:
       "https://akns-images.eonline.com/eol_images/Entire_Site/20171016/rs_600x600-171116115826-600.2.Porsha-Williams.ms.111617.jpg?fit=around|1200:1200&crop=1200:1200;center,top&output-quality=90",
-    scores: [3, 2, 3, 1, 1, 3, 1, 2, 2, 1]
+    scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
   }
 ];
 
