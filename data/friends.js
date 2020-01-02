768b1d207485dbff0d0d620d8dc7ece98af75a7f
Alexis Tanner <alexistanner94@hotmail.com>
2020-01-02 11:11:10 -0500

    added images for friends

--- data/friends.js
+++ data/friends.js
@@ -1,31 +1,32 @@
 var friends = [
   {
-    name: "Lena",
+    name: "Eva",
     photo:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYJtUSKswARugpPWKFD6gTOje0MHsmGDTItdMaR0WTr9PJ5JZ6&s",
+      "https://www.essence.com/wp-content/uploads/2019/03/Screen-Shot-2019-03-19-at-11.28.19-AM-1920x1080.png?width=1920&height=1080",
     scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
   },
   {
-    name: "Zoe",
+    name: "Marlow",
     photo:
-      "https://i.pinimg.com/originals/61/01/7b/61017b275244c90f0ddf6c6fb0a2b319.jpg",
+      "http://thejasminebrand.com/wp-content/uploads/2018/11/Screen-Shot-2018-11-05-at-10.57.56-AM.png",
     scores: [1, 1, 4, 2, 2, 3, 2, 5, 4, 1]
   },
   {
-    name: "Vanessa",
+    name: "Cynthia",
     photo:
-      "https://i.pinimg.com/564x/98/b3/61/98b3614dc634b2e63700ad1cc977d2ea.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQeQBuXT4Y_q0Vvsnzs_c4ilD-VvKOFX04pAIniFgEqlbOoYHCh",
     scores: [2, 3, 1, 4, 1, 3, 1, 5, 1, 1]
   },
   {
-    name: "Pam",
-    photo: "https://miro.medium.com/max/4692/1*zOz85HSX0a8RvJ0DTLeO9g.jpeg",
+    name: "Kandi",
+    photo:
+      "https://akns-images.eonline.com/eol_images/Entire_Site/2019104/rs_1024x759-191104113208-1024-Kandi_Burruss.jpg?fit=inside|900:auto&output-quality=90",
     scores: [3, 4, 5, 2, 3, 5, 5, 4, 4, 2]
   },
   {
-    name: "Kenya",
+    name: "Porshe",
     photo:
-      "https://i.pinimg.com/564x/a0/24/29/a02429961f6923bbffbc7eceacf06f55.jpg",
+      "https://akns-images.eonline.com/eol_images/Entire_Site/20171016/rs_600x600-171116115826-600.2.Porsha-Williams.ms.111617.jpg?fit=around|1200:1200&crop=1200:1200;center,top&output-quality=90",
     scores: [3, 2, 3, 1, 1, 3, 1, 2, 2, 1]
   }
 ];
